d8783a12583cc52a9e4c4393564ba7594a913415
chris houghton and kevin mattison <cehoughton71@gmail.com>
2016-04-25 16:43:08 -0700

    add api

---
+++ app/src/main/java/com/example/guest/willitrain/Constants.java
@@ -0,0 +1,11 @@
+package com.example.guest.willitrain;
+
+/**
+ * Created by Guest on 4/25/16.
+ */
+public class Constants {
+    public static final String WEATHER_API = BuildConfig.WEATHER_API;
+
+    public static final String WEATHER_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?&cnt=7";
+    public static final String WEATHER_CITY_QUERY_PARAMETER = "q";
+}
---
+++ app/src/main/java/com/example/guest/willitrain/WeatherService.java
@@ -0,0 +1,42 @@
+package com.example.guest.willitrain;
+
+
+import android.util.Log;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
+import se.akerfeldt.okhttp.signpost.SigningInterceptor;
+
+import static com.example.guest.willitrain.Constants.WEATHER_CITY_QUERY_PARAMETER;
+
+/**
+ * Created by Guest on 4/25/16.
+ */
+public class WeatherService {
+
+    public static void getWeather(String location, Callback callback) {
+        String WEATHER_API =Constants.WEATHER_API;
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.WEATHER_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(WEATHER_CITY_QUERY_PARAMETER, location);
+        urlBuilder.addQueryParameter("appid", WEATHER_API);
+        Log.d("tag", ""+urlBuilder);
+
+        String url = urlBuilder.build().toString();
+
+        Request request= new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+
+    }
+}
